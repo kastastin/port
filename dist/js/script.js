e0c0d042b8d9e3eda17d058cef2fe0baa69412c0
kastastin <kostya242421@gmai.com>
2021-04-26 16:33:24 +0300

    Seventh commit (form, media, upButton)

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,29 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+// pageup
+
+const upButton = document.querySelector('.up');
+
+function scroll() {
+    const scrolled = window.pageYOffset,
+        height = document.documentElement.clientHeight;
+
+    if (scrolled > 1500) {
+        upButton.classList.add('up-show');
+    } else {
+        upButton.classList.remove('up-show');
+    }
+}
+
+function up() {
+    if (window.pageYOffset > 0) {
+        window.scrollTo({ top: 0,
+            behavior: "smooth"
+        });
+    }
+}
+
+window.addEventListener('scroll', scroll);
+upButton.addEventListener('click', up);
