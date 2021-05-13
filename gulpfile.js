4c8d763d160a9710343060714aa62568c4fc7f47
kastastin <kostya242421@gmai.com>
2021-05-13 20:55:04 +0300

    All from course

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('server', function() {
+gulp.task('server', () => {
 
     browserSync({
         server: {
@@ -18,17 +18,17 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(rename({suffix: '.min',prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
@@ -39,7 +39,7 @@ gulp.task('watch', function() {
 
 gulp.task('html', function () {
     return gulp.src("src/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest("dist/"));
 });
 
